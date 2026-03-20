ccb7ab6fa2c04220704390933b7d00cf6bb6d38e
Marlon Reis <marlonreisdev@gmail.com>
2026-03-19 21:56:34 -0300

    Primeiro commit

---
+++ src/main/java/sissa/NotaZ/NotaZApplication.java
@@ -0,0 +1,13 @@
+package sissa.NotaZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaZApplication.class, args);
+	}
+
+}
